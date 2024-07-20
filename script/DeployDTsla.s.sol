bb7bcb10dfebf0020c97f8dd5aed74e7a5c945be
Juan Carvalho <juanc@LAPTOP-FC26PH2F>
2024-07-20 17:23:48 -0300

    little hotfix on refactoring delpoy file

--- script/DeployDTsla.s.sol
+++ script/DeployDTsla.s.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Script } from "forge-std/Script.sol";
-import { HelperConfig } from "./HelperConfig.sol";
-import { dTSLA } from "../src/dTSLA.sol";
-import { console } from "forge-std/console.sol";
-import { IGetTslaReturnTypes } from "../src/interfaces/IGetTslaReturnTypes.sol";
+import { Script } from 'forge-std/Script.sol';
+import { HelperConfig } from './HelperConfig.sol';
+import { dTSLA } from '../src/dTSLA.sol';
+import { console } from 'forge-std/console.sol';
+import { IGetTslaReturnTypes } from '../src/interfaces/IGetTslaReturnTypes.sol';
 
 contract DeployDTsla is Script {
-    string constant alpacaMintSource = "./functions/sources/alpacaBalance.js";
-    string constant alpacaRedeemSource = "./functions/sources/sellTslaAndSendUsdc.js";
+    string constant alpacaMintSource = './functions/sources/alpacaBalance.js';
+    string constant alpacaRedeemSource = './functions/sources/sellTslaAndSendUsdc.js';
 
     function run() external {
         IGetTslaReturnTypes.GetTslaReturnType memory tslaReturnType = getdTslaRequirements();
@@ -26,7 +26,7 @@ contract DeployDTsla is Script {
 
     function verifyEmptyAttributes(address tslaFeed, address usdcFeed, address functionsRouter, bytes32 donId, uint64 subId) public pure {
         if (tslaFeed == address(0) || usdcFeed == address(0) || functionsRouter == address(0) || donId == bytes32(0) || subId == 0) {
-            revert("something is wrong");
+            revert('DeployDTsla :: verifyEmptyAttributes() :: there is empty attributes');
         }
     }
 
