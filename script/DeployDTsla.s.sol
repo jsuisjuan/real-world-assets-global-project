377f4545932c4bd4629c26ad1a9c984930669654
Juan Carvalho <juanc@LAPTOP-FC26PH2F>
2024-07-20 16:43:44 -0300

    env solved

--- script/DeployDTsla.s.sol
+++ script/DeployDTsla.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import { Script } from "forge-std/Script.sol";
 import { HelperConfig } from "./HelperConfig.sol";
 import { dTSLA } from "../src/dTSLA.sol";
+import { console } from "forge-std/console.sol";
 import { IGetTslaReturnTypes } from "../src/interfaces/IGetTslaReturnTypes.sol";
 
 contract DeployDTsla is Script {
@@ -13,16 +14,27 @@ contract DeployDTsla is Script {
     function run() external {
         // Get params
         IGetTslaReturnTypes.GetTslaReturnType memory tslaReturnType = getdTslaRequirements();
-
+        
         // Actually deploy
-        vm.startBroadcast();
-        deployDTSLA(tslaReturnType.subId, tslaReturnType.mintSource, tslaReturnType.redeemSource, tslaReturnType.functionsRouter, tslaReturnType.donId, tslaReturnType.tslaFeed, tslaReturnType.usdcFeed, tslaReturnType.redemptionCoin, tslaReturnType.secretVersion, tslaReturnType.secretSlot);
-        vm.stopBroadcast();
+        vm.startBroadcast(); // inciando broadcast para enviar transações para a rede blockchain
+        deployDTSLA(        // realiza o deploy do contrato dTSLA
+            tslaReturnType.subId, 
+            tslaReturnType.mintSource, 
+            tslaReturnType.redeemSource, 
+            tslaReturnType.functionsRouter, 
+            tslaReturnType.donId, 
+            tslaReturnType.tslaFeed, 
+            tslaReturnType.usdcFeed, 
+            tslaReturnType.redemptionCoin, 
+            tslaReturnType.secretVersion, 
+            tslaReturnType.secretSlot
+        ); 
+        vm.stopBroadcast(); // finalizando broadcast para enviar transações para uma rede blockchain
     }
 
     function getdTslaRequirements() public returns (IGetTslaReturnTypes.GetTslaReturnType memory) {
         HelperConfig helperConfig = new HelperConfig();
-        (address tslaFeed, address usdcFeed, /*address ethFeed*/, address functionsRouter, bytes32 donId, uint64 subId, address redemptionCoin, , , , uint64 secretVersion, uint8 secretSlot) = helperConfig.activeNetworkConfig();
+        (address tslaFeed, address usdcFeed, /*address ethFeed*/, address functionsRouter, bytes32 donId, uint64 subId, address redemptionCoin, , uint64 secretVersion, uint8 secretSlot) = helperConfig.activeNetworkConfig();
 
         if (tslaFeed == address(0) || usdcFeed == address(0) || functionsRouter == address(0) || donId == bytes32(0) || subId == 0) {
             revert("something is wrong");
