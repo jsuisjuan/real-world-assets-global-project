f66ba0075adf394f3b952076aa0709d15cfe0905
Juan Carvalho <juanc@LAPTOP-FC26PH2F>
2024-07-23 14:05:45 -0300

    refactoreing redeem source code

--- functions/sources/sellTslaAndSendUsdc.js
+++ functions/sources/sellTslaAndSendUsdc.js
@@ -5,186 +5,210 @@ const CRYPTO_TICKER = 'USDCUSD';
 const RWA_CONTRACT = '0x7358D4CDF1c468aA018ec41ddD98b44879a10962';
 const SLEEP_TIME = 5000;
 
+/**
+* Main function that coordinates the trading process.
+* 
+* @returns {Promise<Uint256>} The amount of USDC encoded in Uint256 if successful, otherwise 0.
+*/
 async function main() {
     const amountTsla = args[0];
     const amountUsdc = args[1];
     _checkKeys();
 
-    // sell TSLA for USD
-    let side = 'sell';
-    let [client_order_id, orderStatus, responseStatus] = await placeOrder(ASSET_TICKER, amountTsla, side);
-    if (responseStatus !== 200) {
-        return Functions.encodeUint256(0); 
-    }
-    if (orderStatus !== 'accepted') {
-        return Functions.encodeUint256(0);
-    }
-    let filled = await waitForOrderToFill(client_order_id);
-    if (!filled) {
-        await cancelOrder(client_order_id);
-        return Functions.encodeUint256(0);
-    }
+    let orderFilled = await executeTrade(ASSET_TICKER, amountTsla, 'sell');
+    if (!orderFilled) return Functions.encodeUint256(0);
 
-    // buy USDC with USD
-    side = 'buy';
-    [client_order_id, orderStatus, responseStatus] = await placeOrder(CRYPTO_TICKER, amountTsla, side);
-    if (responseStatus !== 200) {
-        return Functions.encodeUint256(0);
-    }
-    if (orderStatus !== 'accepted') {
-        return Functions.encodeUint256(0);
-    }
-    filled = await waitForOrderToFill(client_order_id);
-    if (!filled) {
-        await cancelOrder(client_order_id);
-        return Functions.encodeUint256(0);
-    }
+    orderFilled = await executeTrade(CRYPTO_TICKER, amountTsla, 'buy');
+    if (!orderFilled) return Functions.encodeUint256(0);
 
-    // send USDC to contract
     const transferId = await sendUsdcToContract(amountUsdc);
-    if (transferId === null) {
-        return Functions.encodeUint256(0);
-    }
-    const completed = await waitForCryptoTransferToComplete(transferId);
-    if (!completed) {
-        return Functions.encodeUint256(0);
-    }
+    if (transferId === null) return Functions.encodeUint256(0);
+    
+    const transferCompleted = await waitForCryptoTransferToComplete(transferId);
+    if (!transferCompleted) return Functions.encodeUint256(0);
+    
     return Functions.encodeUint256(amountUsdc);
 }
 
-async function placeOrder(symbol, qty, side) {
-    const alpacaSellRequest = Functions.makeHttpRequest({
-        'method': 'POST',
-        'url': 'https://paper-api.alpaca.markets/v2/orders',
-        'headers': {
-            'accept': 'application/json',
-            'content-type': 'application/json',
-            'APCA-API-KEY-ID': secrets.alpacaKey,
-            'APCA-API-SECRET-KEY': secrets.alpacaSecret
-        },
-        'data': {
-            'side': side,
-            'type': 'market',
-            'time_in_force': 'gtc',
-            'symbol': symbol,
-            'qty': qty
-        }
-    });
-    const [response] = await Promise.all([alpacaSellRequest]);
-    const responseStatus = response.status;
-
-    console.log(`\nResponse status: ${responseStatus}\n`);
-    console.log(response);
-    console.log(`\n`);
-
-    const { client_order_id, 'status': orderStatus } = response.data;
-    return client_order_id, orderStatus, responseStatus;
-}
-
-async function cancelOrder(client_order_id) {
-    const alpacaCancelRequest = Functions.makeHttpRequest({
-        'method': 'DELETE',
-        'url': `https://paper-api.alpaca.markets/v2/orders/${client_order_id}`,
-        'headers': {
-            'accept': 'application/json',
-            'APCA-API-KEY-ID': secrets.alpacaKey,
-            'APCA-API-SECRET-KEY': secrets.alpacaSecret
-        }
-    });
-    const [response] = await Promise.all([alpacaCancelRequest]);
-    const responseStatus = response.status;
-    return responseStatus;
-}
-
-async function waitForOrderToFill(client_order_id) {
-    const capNumberOfSleeps = 10;
-    let numberOfSleeps = 0;
-    let filled = false;
-    while (numberOfSleeps < capNumberOfSleeps) {
-        const alpacaOrderStatusRequest = Functions.makeHttpRequest({
-            'method': 'GET',
-            'url': `https://paper-api.alpaca.markets/v2/orders/${client_order_id}`,
-            'headers': {
-                'accept': 'application/json',
-                'APCA-API-KEY-ID': secrets.alpacaKey,
-                'APCA-API-SECRET-KEY': secrets.alpacaSecret
-            }
-        });
-        const [response] = await Promise.all([alpacaOrderStatusRequest]);
-        const responseStatus = response.status;
-        const { 'status': orderStatus } = response.data;
-        if (responseStatus !== 200) {
-            return false;
-        }
-        if (orderStatus === 'filled') {
-            filled = true;
-            break;
-        }
-        numberOfSleeps++;
-        await sleep(SLEEP_TIME);
-    }
+/**
+* Executes a trade by placing an order and waiting for it to fill.
+* 
+* @param {string} ticker - The ticker symbol of the asset.
+* @param {string} amount - The amount to trade.
+* @param {string} side - The side of the trade ('buy' or 'sell').
+* @returns {Promise<boolean>} True if the order was filled, otherwise false.
+*/
+async function executeTrade(ticker, amount, side) {
+    const [client_order_id, orderStatus, responseStatus] = await placeOrder(ticker, amount, side);
+    if (responseStatus !== 200 || orderStatus !== 'accepted') return false;
+    const filled = await waitForOrderToFill(client_order_id);
+    if (!filled) await cancelOrder(client_order_id);
     return filled;
 }
 
-async function sendUsdcToContract(usdcAmount) {
-    const transferRequest = Functions.makeHttpRequest({
-        'method': 'POST',
-        'url': 'https://paper-api.alpaca.markets/v2/wallets/transfers',
-        'headers': {
-            'accept': 'application/json',
+/**
+* Places an order on the Alpaca API.
+* 
+* @param {string} symbol - The ticker symbol of the asset.
+* @param {string} qty - The quantity to trade.
+* @param {string} side - The side of the trade ('buy' or 'sell').
+* @returns {Promise<[string, string, number]>} The client order ID, order status, and response status.
+*/
+async function placeOrder(symbol, qty, side) {
+    const alpacaSellRequest = Functions.makeHttpRequest({
+        method: 'POST',
+        url: 'https://paper-api.alpaca.markets/v2/orders',
+        headers: {
+            accept: 'application/json',
             'content-type': 'application/json',
             'APCA-API-KEY-ID': secrets.alpacaKey,
             'APCA-API-SECRET-KEY': secrets.alpacaSecret
         },
-        'data': {
-            'amount': usdcAmount,
-            'address': RWA_CONTRACT,
-            'asset': CRYPTO_TICKER
-        }
+        data: { side, type: 'market', time_in_force: 'gtc', symbol, qty }
     });
-    const [response] = await Promise.all([transferRequest]);
-    if (response.status !== 200) {
-        return null;
-    }
-    return response.data.id;
+    const response = await alpacaSellRequest;
+    const responseStatus = response.status;
+    const { client_order_id, status: orderStatus } = response.data;
+    return client_order_id, orderStatus, responseStatus;
 }
 
+/**
+* Cancels an order on the Alpaca API.
+* 
+* @param {string} client_order_id - The client order ID.
+* @returns {Promise<number>} The response status.
+*/
+async function cancelOrder(client_order_id) {
+    const alpacaCancelRequest = Functions.makeHttpRequest({
+        method: 'DELETE',
+        url: `https://paper-api.alpaca.markets/v2/orders/${client_order_id}`,
+        headers: {
+            accept: 'application/json',
+            'APCA-API-KEY-ID': secrets.alpacaKey,
+            'APCA-API-SECRET-KEY': secrets.alpacaSecret
+        }
+    });
+    const response = await alpacaCancelRequest;
+    return response.status;
+}
+
+/**
+* Waits for an order to fill by repeatedly checking its status.
+* 
+* @param {string} client_order_id - The client order ID.
+* @returns {Promise<boolean>} True if the order was filled, otherwise false.
+*/
+async function waitForOrderToFill(client_order_id) {
+    const capNumberOfSleeps = 10;
+    let numberOfSleeps = 0;
+    while (numberOfSleeps < capNumberOfSleeps) {
+        const orderStatus = await checkOrderStatus(client_order_id);
+        if (orderStatus === 'filled') return true;
+        numberOfSleeps++;
+        await sleep(SLEEP_TIME);
+    }
+    return false;
+}
+
+/**
+* Checks the status of an order on the Alpaca API.
+* 
+* @param {string} client_order_id - The client order ID.
+* @returns {Promise<string>} The order status.
+*/
+async function checkOrderStatus(client_order_id) {
+    const alpacaOrderStatusRequest = Functions.makeHttpRequest({
+        method: 'GET',
+        url: `https://paper-api.alpaca.markets/v2/orders/${client_order_id}`,
+        headers: {
+            accept: 'application/json',
+            'APCA-API-KEY-ID': secrets.alpacaKey,
+            'APCA-API-SECRET-KEY': secrets.alpacaSecret
+        }
+    });
+    const response = await alpacaOrderStatusRequest;
+    const { status: orderStatus } = response.data;
+    return orderStatus;
+}
+
+/**
+* Sends USDC to the specified contract.
+* 
+* @param {string} usdcAmount - The amount of USDC to send.
+* @returns {Promise<string|null>} The transfer ID if successful, otherwise null.
+*/
+async function sendUsdcToContract(usdcAmount) {
+    const transferRequest = Functions.makeHttpRequest({
+        method: 'POST',
+        url: 'https://paper-api.alpaca.markets/v2/wallets/transfers',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            'APCA-API-KEY-ID': secrets.alpacaKey,
+            'APCA-API-SECRET-KEY': secrets.alpacaSecret
+        },
+        data: { amount: usdcAmount, address: RWA_CONTRACT, asset: CRYPTO_TICKER }
+    });
+    const response = await transferRequest;
+    return response.status === 200 ? response.data.id : null;
+}
+
+/**
+* Waits for a crypto transfer to complete by repeatedly checking its status.
+* 
+* @param {string} transferId - The transfer ID.
+* @returns {Promise<boolean>} True if the transfer was completed, otherwise false.
+*/
 async function waitForCryptoTransferToComplete(transferId) {
     const capNumberOfSleeps = 120;
     let numberOfSleeps = 0;
-    let completed = false;
     while (numberOfSleeps < capNumberOfSleeps) {
-        const alpacaTransferStatusRequest = Functions.makeHttpRequest({
-            'method': 'GET',
-            'url': `https://paper-api.alpaca.markets/v2/wallets/transfers/${transferId}`,
-            'headers': {
-                'accept': 'application/json',
-                'APCA-API-KEY-ID': secrets.alpacaKey,
-                'APCA-API-SECRET-KEY': secrets.alpacaSecret
-            }
-        });
-        const [response] = await Promise.all([alpacaTransferStatusRequest]);
-        const responseStatus = response.status;
-        const { 'status': transferStatus } = response.data;
-        if (responseStatus !== 200) {
-            return false;
-        }
-        if (transferStatus === 'completed') {
-            completed = true;
-            break;
-        }
+        const transferStatus = await checkTransferStatus(transferId);
+        if (transferStatus === 'completed') return true;
         numberOfSleeps++;
         await sleep(SLEEP_TIME); 
     }
-    return completed;
+    return false;
 }
 
+/**
+* Checks the status of a crypto transfer on the Alpaca API.
+* 
+* @param {string} transferId - The transfer ID.
+* @returns {Promise<string>} The transfer status.
+*/
+async function checkTransferStatus(transferId) {
+    const alpacaTransferStatusRequest = Functions.makeHttpRequest({
+        method: 'GET',
+        url: `https://paper-api.alpaca.markets/v2/wallets/transfers/${transferId}`,
+        headers: {
+            accept: 'application/json',
+            'APCA-API-KEY-ID': secrets.alpacaKey,
+            'APCA-API-SECRET-KEY': secrets.alpacaSecret
+        }
+    });
+    const response = await alpacaTransferStatusRequest;
+    const { status: transferStatus } = response.data;
+    return transferStatus;
+} 
+
+/**
+* Checks if the Alpaca API keys are set.
+* 
+* @throws {Error} If the Alpaca API keys are not set.
+*/
 function _checkKeys() {
-    if (secrets.alpacaKey === '' || secrets.alpacaSecret === '') {
+    if (!secrets.alpacaKey || !secrets.alpacaSecret) {
         throw Error('need alpaca keys');
     }
 }
 
+/**
+* Sleeps for a specified amount of milliseconds.
+* 
+* @param {number} ms - The number of milliseconds to sleep.
+* @returns {Promise<void>} A promise that resolves after the specified time.
+*/
 function sleep(ms) {
     return new Promise(resolve => setTimeout(() => resolve, ms));
 }
